9e475ccc1a9ff9b80544298663602577c3156499
ChristianP-7 <christianperezt7@gmail.com>
2024-04-18 19:02:31 -0500

    Integracion front con back

--- gestion-clientes-frontend/src/services/ClienteService.js
+++ gestion-clientes-frontend/src/services/ClienteService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const CLIENTE_BASE_REST_API_URL="http://localhost:8080/api/v1/clientes";
+const CLIENTE_BASE_REST_API_URL="https://clientes-gestion.onrender.com/api/v1";
 
 class ClienteService{
 
