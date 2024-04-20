7d527dbca295edb120d035b4c57c827bda318488
ChristianP-7 <christianperezt7@gmail.com>
2024-04-19 21:40:51 -0500

    Abriendo API

---
+++ gestion-clientes-backend/src/main/java/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // Permitir desde cualquier origen
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                        .allowedHeaders("*") // Encabezados permitidos
+                        .exposedHeaders("Authorization") // Encabezados expuestos
+                        .allowCredentials(true) // Permitir credenciales
+                        .maxAge(3600); // Tiempo máximo de validez de la solicitud preflight (OPTIONS)
+            }
+        };
+    }
+}
