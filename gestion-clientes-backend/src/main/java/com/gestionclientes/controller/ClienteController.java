e76de42fe41b2971181a102980fbc999904ec1e0
ChristianP-7 <christianperezt7@gmail.com>
2024-04-19 21:21:13 -0500

    Correción de urls

--- gestion-clientes-backend/src/main/java/com/gestionclientes/controller/ClienteController.java
+++ gestion-clientes-backend/src/main/java/com/gestionclientes/controller/ClienteController.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "https://clientes-gestion.onrender.com/api/v1/clientes")
+@CrossOrigin(origins = "clientes-gestion-8w3l9v6b4-christian-perezs-projects-f5e0eacb.vercel.app")
 @RestController
 @RequestMapping("/api/v1")
 public class ClienteController {
